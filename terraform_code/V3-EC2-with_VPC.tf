be0b2d28880e144409289d20f9f60a87684c2faa
Syed Moulali Basha <syedmoulalibasha88@gmail.com>
2024-10-02 12:53:31 +0530

    Update V3-EC2-with_VPC.tf
    subnet cidr_block changed

--- terraform_code/V3-EC2-with_VPC.tf
+++ terraform_code/V3-EC2-with_VPC.tf
@@ -47,7 +47,7 @@ resource "aws_vpc" "dppj-vpc" {
 
 resource "aws_subnet" "dppj-public-subnet-01" {
   vpc_id = aws_vpc.dppj-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1a"
   tags = {
@@ -57,7 +57,7 @@ resource "aws_subnet" "dppj-public-subnet-01" {
 
 resource "aws_subnet" "dppj-public-subnet-02" {
   vpc_id = aws_vpc.dppj-vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
   tags = {
