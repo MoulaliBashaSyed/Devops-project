82cb8ddc9ed84f8b1e804a5c4b3e753e47a1ba92
Syed Moulali Basha <syedmoulalibasha88@gmail.com>
2024-10-02 12:37:06 +0530

    adding v3 version

---
+++ terraform_code/V3-EC2-with_VPC.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name = "devopsp"
+  //security_groups = [ "demo-sg" ]
+  vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
+  subnet_id = aws_subnet.dppj-public-subnet-01.id
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dppj-vpc.id
+  
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "SSH-prot"
+
+  }
+}
+
+resource "aws_vpc" "dppj-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dppj-vpc"
+  }
+}
+
+resource "aws_subnet" "dppj-public-subnet-01" {
+  vpc_id = aws_vpc.dppj-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dpp-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "dppj-public-subnet-02" {
+  vpc_id = aws_vpc.dppj-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "dpp-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "dppj-igw" {
+  vpc_id = aws_vpc.dppj-vpc.id
+  tags = {
+    Name = "dppj-igw"
+  }
+}
+
+resource "aws_route_table" "dppj-public-rt" {
+  vpc_id = aws_vpc.dppj-vpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dppj-igw.id
+  }
+}
+
+resource "aws_route_table_association" "dppj-rta-public-subnet-01" {
+  subnet_id = aws_subnet.dppj-public-subnet-01.id
+  route_table_id = aws_route_table.dppj-public-rt.id
+}
+
+resource "aws_route_table_association" "dppj-rta-public-subnet-02" {
+  subnet_id = aws_subnet.dppj-public-subnet-02.id
+  route_table_id = aws_route_table.dppj-public-rt.id
+}
