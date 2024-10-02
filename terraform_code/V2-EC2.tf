0374c359b96bbaa0ec49321701f267191cabdbee
Syed Moulali Basha <syedmoulalibasha88@gmail.com>
2024-10-02 12:10:43 +0530

    adding v2 version

---
+++ terraform_code/V2-EC2.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name = "devopsp"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "SSH-prot"
+
+  }
+}
